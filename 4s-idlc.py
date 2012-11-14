0906f5fa677a799809262208b16f4ff75e801938
Corey Harper <corey.harper@gmail.com>
2012-11-14 12:32:07 -0600

    adding code to populate 4store from id.loc.gov

---
+++ 4s-idlc.py
@@ -0,0 +1,87 @@
+import codecs
+import urllib
+import time
+from urllib import urlopen
+from pprint import pprint
+from HTTP4Store import HTTP4Store
+
+import xml.etree.cElementTree as ET
+
+
+matches = codecs.open('/media/Storage/EAD for Corey/EAD for Corey/idlc.matches.txt', 'r', 'utf-8')
+
+store = HTTP4Store('http://localhost:8080')
+
+r = store.add_graph("http://example.org/idlc", "", "xml")
+pprint(r)
+count = 0	
+
+for line in matches:
+	m = line.split("|")[5].rstrip()
+	count += 1
+	if m != "None":
+		m = m + ".rdf"
+		#url = urlopen("http://id.loc.gov/authorities/subjects/sh85137025.rdf")
+		url = urlopen(m)
+		raw = url.read().decode('utf-8')
+		#xml = ET.fromstring(raw)
+		#r = store.del_graph("http://example.org/idlc")
+		r =  store.append_graph("http://example.org/idlc", raw.encode('utf-8'), "xml")
+		print str(count) + ": " + str(r.status)
+		time.sleep(2)
+
+
+
+#r =  store.append_graph("http://example.org/idlc", raw, "xml")
+#print r.status
+#time.sleep(2)
+#r = e.del_graph("http://example.org/relsext")
+
+#url = "http://id.loc.gov/authorities/subjects/sh85137025.rdf"
+#xml = ET.parse(urllib.urlopen(url))
+#url = urlopen("http://id.loc.gov/authorities/subjects/sh85137025.rdf")
+#raw = url.read().decode('utf-8')
+#xml = ET.fromstring(raw)
+#print(xml)
+#print(xml.findall("a"))        
+
+
+
+
+#xml = ET.ElementTree(urllib.urlopen(url))
+#print "Root: " + str(xml.getroot())
+#for child in xml.getroot():
+#	print child.tag
+#tree = ET.ElementTree(file=f)
+#print type(raw)
+#print dir(raw)
+
+#print xml.Element("root")
+#print ET.tostring(xml)
+
+#print xml
+
+#r = store.del_graph("http://example.org/idlc")
+#print r.status
+
+
+#r =  store.append_graph("http://example.org/idlc", raw, "xml")
+#print r.status
+#time.sleep(2)
+#r = e.del_graph("http://example.org/relsext")
+
+
+q = """LOAD <http://id.loc.gov/authorities/subjects/sh85137025.rdf>
+INTO GRAPH <http://example.org/idlc>
+"""
+
+#pprint(store.sparql(q))
+
+#for line in matches:
+#	count += 1
+	#r =  store.append_graph("http://example.org/idlc", line, "xml")
+
+#		out.write(ET.tostring(rec) + '\n')
+
+	
+#	print str(count) + ": " + str(r.status)
